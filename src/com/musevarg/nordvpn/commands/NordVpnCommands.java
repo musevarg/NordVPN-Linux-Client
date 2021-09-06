8aa165440f40bddd1b48c1c3675a27cefdc02829
musevarg-linux <gravesum5@gmail.com>
2021-09-06 19:20:45 +0200

    implemented quick connect command and relevant test

--- src/com/musevarg/nordvpn/commands/NordVpnCommands.java
+++ src/com/musevarg/nordvpn/commands/NordVpnCommands.java
@@ -5,6 +5,9 @@ package com.musevarg.nordvpn.commands;
 * We do not need to instantiate it more than once.
 */
 
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+
 public class NordVpnCommands {
 
     // Static variable reference of single_instance
@@ -17,8 +20,79 @@ public class NordVpnCommands {
     // Static method to create instance of Singleton class
     public static NordVpnCommands getInstance()
     {
-        if (single_instance == null)
+        if (single_instance == null) {
             single_instance = new NordVpnCommands();
+        }
         return single_instance;
+    }
+
+    public String connect(){
+        String response;
+        try{
+            response = runCommand("nordvpn c");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = "Something went wrong";
+        }
+        return response;
+    }
+
+    private static String runCommand(String command) throws Exception{
+        // Let the user know which command is running
+        System.out.println("Running '" + command + "'");
+        // Create a process builder to run command
+        ProcessBuilder builder = new ProcessBuilder();
+        // Set the command to run in a Linux shell
+        builder.command("sh", "-c", command);
+        // Run command
+        Process process = builder.start();
+
+        // Create the streams that will read responses from the shell
+        OutputStream outputStream = process.getOutputStream();
+        InputStream inputStream = process.getInputStream();
+        InputStream errorStream = process.getErrorStream();
+
+        String response;
+
+        // Print responses to the user
+        response = printStream(inputStream);
+        printStream(errorStream);
+
+        boolean isFinished = process.waitFor(30, TimeUnit.SECONDS);
+        outputStream.flush();
+        outputStream.close();
+
+        if(!isFinished) {
+            process.destroyForcibly();
+        }
+        return response;
+    }
+
+    // Method to print response from the shell
+    private static String printStream(InputStream inputStream) throws IOException {
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            String lastLine = "";
+            while((line = bufferedReader.readLine()) != null) {
+                if (!isLoadingCharacter(line)){
+                    System.out.println(line);
+                    lastLine = line;
+                }
+            }
+            return lastLine;
+        }
+    }
+
+    // Remove the loading characters from the response
+    // Loading characters are responses containing one of \|/- followed by a space
+    // They have a string length of 2 (e.g. "\ " or "| ")
+    private static Boolean isLoadingCharacter(String line){
+        int loadingCharactersLength = 2;
+        return line.length() <= loadingCharactersLength;
+    }
+
+    // Check if response contains "you are connected"
+    private static Boolean isConnected(String response){
+        return response.toLowerCase().contains("you are connected");
     }
 }
--- src/com/musevarg/nordvpn/commands/NordVpnCommandsTest.java
+++ src/com/musevarg/nordvpn/commands/NordVpnCommandsTest.java
@@ -1,5 +1,6 @@
 package com.musevarg.nordvpn.commands;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,6 +12,18 @@ class NordVpnCommandsTest {
         NordVpnCommands commands1 = NordVpnCommands.getInstance();
         NordVpnCommands commands2 = NordVpnCommands.getInstance();
         assertEquals(commands1.hashCode(), commands2.hashCode());
+    }
+
+    @Test //Run quick connect command
+    public void runCommandTest(){
+        NordVpnCommands commands = NordVpnCommands.getInstance();
+        String response = commands.connect();
+        assertTrue(isConnected(response));
+    }
+
+    // Check if response contains "you are connected"
+    private Boolean isConnected(String response){
+        return response.toLowerCase().contains("you are connected");
     }
 
 }
