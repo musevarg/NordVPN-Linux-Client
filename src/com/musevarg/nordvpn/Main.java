bcfdb52e6d81f596b92fe92de5b17f4ecd55c6bb
musevarg-linux <gravesum5@gmail.com>
2021-09-06 23:49:56 +0200

    implemented basic UI

--- src/com/musevarg/nordvpn/Main.java
+++ src/com/musevarg/nordvpn/Main.java
@@ -1,8 +1,12 @@
 package com.musevarg.nordvpn;
 
+import com.musevarg.nordvpn.ui.Client;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main (String[] args){
-
+        SwingUtilities.invokeLater(Client::new);
     }
 }
--- src/com/musevarg/nordvpn/ui/Client.java
+++ src/com/musevarg/nordvpn/ui/Client.java
@@ -3,18 +3,45 @@ package com.musevarg.nordvpn.ui;
 import com.musevarg.nordvpn.vpn.NordVPN;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Client extends javax.swing.JPanel {
+public class Client extends JFrame {
 
     private JButton connectButton;
     private JLabel statusLabel;
+    private JPanel mainPanel;
     private NordVPN nordVPN = NordVPN.getInstance();
 
     public Client(){
         initComponents();
+        this.setSize(175, 250);
+        this.setContentPane(mainPanel);
+        this.setVisible(true);
     }
 
     private void initComponents(){
+        updateUiComponents();
 
+        connectButton.addActionListener(e -> {
+            if (nordVPN.isConnected){
+                nordVPN.disconnect();
+            } else {
+                nordVPN.connect();
+            }
+            updateUiComponents();
+        });
     }
+
+    private void updateUiComponents(){
+        statusLabel.setText("Status: " + nordVPN.status());
+        nordVPN.isConnected = nordVPN.status().equals("Connected");
+        if(nordVPN.isConnected){
+            connectButton.setText("Disconnect");
+        } else {
+            connectButton.setText("Connect");
+        }
+    }
+
+
 }
