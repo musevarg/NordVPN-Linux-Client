08bf1ca5c452e6965b1946c64452f02238ed6a7e
musevarg <gravesum5@gmail.com>
2021-09-09 16:28:25 +0200

    typo

--- src/com/musevarg/nordvpn/util/UtilTest.java
+++ src/com/musevarg/nordvpn/util/UtilTest.java
@@ -8,20 +8,20 @@ class UtilTest {
 
     @Test // Get Belgium country code
     public void getBelgiumCountryCode(){
-        String belgiumCode = CountryLocales.getCountryCode("Belgium");
-        assertEquals("BE", belgiumCode);
+        String code = CountryLocales.getCountryCode("Belgium");
+        assertEquals("BE", code);
     }
 
     @Test // Get US country code
     public void getUSCountryCode(){
-        String belgiumCode = CountryLocales.getCountryCode("United States");
-        assertEquals("US", belgiumCode);
+        String code = CountryLocales.getCountryCode("United States");
+        assertEquals("US", code);
     }
 
     @Test // Get South Africa country code
     public void getZACountryCode(){
-        String belgiumCode = CountryLocales.getCountryCode("South Africa");
-        assertEquals("ZA", belgiumCode);
+        String code = CountryLocales.getCountryCode("South Africa");
+        assertEquals("ZA", code);
     }
 
 }
