555b1ffda679c5fc00286c91e88bddd0e638d824
musevarg <gravesum5@gmail.com>
2021-09-09 14:28:44 +0200

    give the UI a native look and feel

--- src/com/musevarg/nordvpn/Main.java
+++ src/com/musevarg/nordvpn/Main.java
@@ -6,7 +6,8 @@ import javax.swing.*;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); // Gives UI a native look and feel
         SwingUtilities.invokeLater(Client::new);
     }
 }
